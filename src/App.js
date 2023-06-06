c09d838f063878542b0afa6dc7dc12b0627dcaf9
IshaJain1408 <116328120+IshaJain1408@users.noreply.github.com>
2023-06-06 16:33:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  // const React = require('react'); 
+// const ReactDOM = require('react-dom'); 
+// const client = require('./client'); 
+class App extends React.Component {
+
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {employees: []};
+	// }
+
+	// componentDidMount() { 
+	// 	client({method: 'GET', path: '/api/employees'}).done(response => {
+	// 		this.setState({employees: response.entity._embedded.employees});
+	// 	});
+	// }
+
+	render() { 
+	
+			
+
+		return (
+			<table>
+				<tbody>
+					<tr>
+          <td>{this.props.employee.firstName}</td>
+				<td>{this.props.employee.lastName}</td>
+				<td>{this.props.employee.description}</td>
+
+					</tr>
+					
+				</tbody>
+			</table>
+		)
+	}
 }
+
 
 export default App;
